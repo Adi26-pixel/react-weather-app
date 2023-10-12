106cbc3e34f7cc1e8725e6c8d687184fd30c5a22
Adeline Monsengo <adeline.monsengo@gmail.com>
2023-10-12 12:48:38 +0200

    Added the git rebo url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Adeline and is{" "}
+        <a
+          href="https://github.com/Adi26-pixel/react-weather-app.git"
+          target="_blank"
+          rel="noreferrer">
+          open-sourced on github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
