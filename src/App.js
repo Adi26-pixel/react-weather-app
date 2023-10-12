36c2ddf7fc678ab3e5b18047b5f30c83f81b6634
Adeline Monsengo <adeline.monsengo@gmail.com>
2023-10-12 12:59:12 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Adeline and is{" "}
-        <a
-          href="https://github.com/Adi26-pixel/react-weather-app.git"
-          target="_blank"
-          rel="noreferrer">
-          open-sourced on github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Adeline and is{" "}
+          <a
+            href="https://github.com/Adi26-pixel/react-weather-app.git"
+            target="_blank"
+            rel="noreferrer">
+            open-sourced on github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
